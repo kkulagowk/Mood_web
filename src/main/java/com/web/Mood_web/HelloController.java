080ca24185b9e99f1edc25d275292df2125894e0
kkulagow <01178664@pw.edu.pl>
2023-12-28 00:17:53 +0100

    "end point added"

--- src/main/java/com/web/Mood_web/HelloController.java
+++ src/main/java/com/web/Mood_web/HelloController.java
@@ -1,7 +1,10 @@
 package com.web.Mood_web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import  java.util.HashMap;
 
 @RestController
 public class HelloController {
@@ -9,6 +12,14 @@ public class HelloController {
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @PostMapping("/some/data")
+    public HashMap<String, String> returnData(){
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("firstAttribut", "Piece of data");
+        map.put("secondAttribut", "More data");
+        return map;
     }
 
 }
